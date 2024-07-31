a2fbccc0e7185e51a4ef8ec3f5203ba6a5986d76
Helio-Centrism <heliocentrism33@gmail.com>
2024-07-31 17:28:00 +0530

    day1

---
+++ Beigner level/band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city in which you grew up?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name could be " + pet + " " + city)
